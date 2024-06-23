2242f81419903aad4816afdc4b98e8b0ac706940
denduednue <124910849+denduednue@users.noreply.github.com>
2024-06-22 21:26:27 -0700

    sand

---
+++ wall.js
@@ -0,0 +1,9 @@
+elements.funny_wall = {
+    color: "#dddd00",
+    behavior: behaviors.WALL,
+    ignore: ["ecloner","slow_cloner","clone_powder","floating_cloner","wall","ewall"],
+    category:"mods",
+    insulate:true,
+    hardness: 1,
+    darkText: true,
+},
