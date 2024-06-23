b5515e9ada5f282018725470f69863d330c76986
denduednue <124910849+denduednue@users.noreply.github.com>
2024-06-22 21:30:37 -0700

    more mods bc github wont let me uplaod them all at once

---
+++ mods/nuke.js
@@ -0,0 +1,13 @@
+elements.dehydrated_nuke = {
+    color: "#534636",
+    behavior: [
+        "XX|XX|XX",
+        "XX|XX|XX",
+        "M2|M1 AND EX:60>plasma,plasma,plasma,plasma,radiation,rad_steam|M2",
+    ],
+    category: "weapons",
+    state: "solid",
+    density: 1500,
+    excludeRandom: true,
+    cooldown: defaultCooldown
+},
